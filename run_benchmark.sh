1ea81af07913488ee7eea68aaec688be423fe8c7
johnnynunez <johnnynuca14@gmail.com>
2023-05-31 19:20:22 +0200

    .

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # This script is used to install the torchbenchmark environment and run the first run
+docker build -t pytorch_benchmark -f ./docker/Dockerfile .
+docker run --rm -it --gpus all --name pytorch_benchmark -v /workspace:/workspace pytorch_benchmark
+# Run the benchmark
+cd /workspace/benchmark
 pytest test_bench.py --benchmark-autosave --ignore_machine_config
