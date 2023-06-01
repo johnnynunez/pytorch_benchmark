c7e6e5cb6af8baa7d345e5c8c2adccb8044f9dd6
johnnynunez <johnnynuca14@gmail.com>
2023-06-01 14:02:52 +0200

    .

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script is used to install the torchbenchmark environment and run the first run
 docker build --build-arg CUDA_VERSION=12.1 -t pytorch_benchmark .
-sudo docker run --rm -it --gpus all --name pytorch_benchmark -v /workspace:/workspace -e RUNNER_NAME=my_runner pytorch_benchmark
+docker run --rm -it --gpus all --name pytorch_benchmark -v /workspace:/workspace pytorch_benchmark
 
 # Run the benchmark
 cd /workspace/benchmark
