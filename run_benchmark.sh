21914f580a6831691088e734ce913c57b29586e7
johnnynunez <johnnynuca14@gmail.com>
2023-06-01 14:55:46 +0200

    .

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
 # This script is used to install the torchbenchmark environment and run the first run
-docker build --build-arg CUDA_VERSION=12.1 -t pytorch_benchmark .
-docker run --rm -it --gpus all --name pytorch_benchmark -v /workspace:/workspace pytorch_benchmark
+# ./script.sh <name_ngc> <name_dataset> <name_config> <name_model> <timeout>
 
-# Run the benchmark
-cd /workspace/benchmark
-pytest test_bench.py --benchmark-autosave --ignore_machine_config
+# Set default values for arguments
+CUDA_VERSION="${1:-12.1}"
+NAME_NGC="${2:-pytorch:23.05-py3}"
+NAME_DATASET="${3:-all}"
+NAME_CONFIG="${4:-AdaA6000_v1}"
+NAME_MODEL="${5:-all}"
+TIME_OUT="${6:-3000}"
+
+docker build --build-arg CUDA_VERSION=$CUDA_VERSION -t pytorch_benchmark -f docker/Dockerfile .
+
+# Run the benchmark inside the container
+docker run --gpus all -it -v "${PWD}:/prueba" pytorch_benchmark /bin/bash -c "cd benchmark && eval \"\$(conda shell.bash hook)\" && conda activate torchbench && pytest test_bench.py --benchmark-autosave --ignore_machine_config"
+
+export NAME_NGC="$NAME_NGC"
+export NAME_DATASET="$NAME_DATASET"
+
+wget https://raw.githubusercontent.com/lambdal/deeplearning-benchmark/master/pytorch/setup.sh && \
+chmod +x setup.sh && \
+./setup.sh $NAME_NGC
+
+cd deeplearning-benchmark/pytorch && \
+docker run --gpus all --rm --shm-size=64g \
+-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+-v ~/data:/data \
+-v $(pwd)"/scripts":/scripts \
+nvcr.io/nvidia/${NAME_NGC} \
+/bin/bash -c "cp -r /scripts/* /workspace;  ./run_prepare.sh $NAME_DATASET"
+
+export NAME_NGC="$NAME_NGC"
+export NAME_CONFIG="$NAME_CONFIG"
+export NAME_MODEL="$NAME_MODEL"
+export TIME_OUT="$TIME_OUT"
+
+docker run \
+	--rm --shm-size=1024g \
+	--gpus all \
+	-v ~/DeepLearningExamples/PyTorch:/workspace/benchmark \
+	-v ~/data:/data \
+	-v $(pwd)"/scripts":/scripts \
+	-v $(pwd)"/results":/results \
+	nvcr.io/nvidia/${NAME_NGC} \
+	/bin/bash -c "cp -r /scripts/* /workspace; ./run_benchmark.sh $NAME_CONFIG $NAME_MODEL $TIME_OUT"
