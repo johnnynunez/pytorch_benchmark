95718ccb6ee80388457fe200ccb7a4917d567013
johnnynunez <johnnynuca14@gmail.com>
2023-06-06 20:23:02 +0200

    .

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -3,7 +3,7 @@
 # ./script.sh <name_ngc> <name_dataset> <name_config> <name_model> <timeout>
 
 # Set default values for arguments
-CUDA_VERSION="${1:-12.1}"
+CUDA_VERSION="${1:-11.8}"
 NAME_NGC="${2:-pytorch:23.05-py3}"
 NAME_DATASET="${3:-all}"
 NAME_CONFIG="${4:-AdaA6000_v1}"
@@ -15,6 +15,7 @@ docker build --build-arg CUDA_VERSION=$CUDA_VERSION -t pytorch_benchmark -f dock
 # Run the benchmark inside the container
 # docker run --gpus all -it -v "${PWD}:/prueba" pytorch_benchmark /bin/bash -c "cd benchmark && eval \"\$(conda shell.bash hook)\" && conda activate torchbench && pytest test_bench.py --benchmark-autosave --benchmark-json ~/prueba --ignore_machine_config --cuda_only"
 
+# docker run --gpus all -it -v "${PWD}/workspace:/workspace" pytorch_benchmark /bin/bash -c "cd /workspace/benchmark && eval \"\$(conda shell.bash hook)\" && conda activate torchbench && python run_benchmark.py torch-nightly -d cuda -t train,eval"
 docker run --gpus all -it -v "${PWD}:/prueba" pytorch_benchmark /bin/bash -c "cd benchmark && eval \"\$(conda shell.bash hook)\" && conda activate torchbench && python run_benchmark.py torch-nightly -d cuda -t train,eval"
 
 export NAME_NGC="$NAME_NGC"
