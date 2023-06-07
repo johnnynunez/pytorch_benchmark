5af0759c95cfa2c4a066f4a15ce324bad4e4c572
johnnynunez <johnnynuca14@gmail.com>
2023-06-07 17:07:41 +0200

    .

---
+++ tensorflow/run_bench.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script is used to install the torchbenchmark environment and run the first run
+# ./script.sh <name_ngc> <name_dataset> <name_config> <name_model> <timeout>
+git clone https://github.com/johnnynunez/benchmarks.git
+cd benchmarks
+python3 perfzero/lib/setup.py --dockerfile_path=docker/Dockerfile_ubuntu_2204_tf_cuda_12
+docker run --gpus all -it -v $(pwd):/workspace -v /data:/data perfzero/tensorflow bash
+
+python3 /workspace/perfzero/lib/benchmark.py \
+--git_repos="https://github.com/johnnynunez/models.git;benchmark" \
+--python_path=models \
+--gcloud_key_file_url="" \
+--python_path=models \
+--benchmark_methods=official.r1.resnet.estimator_benchmark.Resnet50EstimatorBenchmarkSynth.benchmark_graph_1_gpu \
+--root_data_dir=/data
